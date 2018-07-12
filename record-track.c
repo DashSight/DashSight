6389689d47a7e912fa73d4bf24e2d236719aa2c7
Alistair Francis <alistair@alistair23.me>
2018-07-11 21:53:57 -0700

    record-track: Print out some data
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- record-track.c
+++ record-track.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <gps.h>
 #include "common.h"
 #include "record-track.h"
@@ -26,6 +27,8 @@ void record_track(cmd_args args)
 {
 	FILE *fd;
 	struct gps_data_t gps_data;
+	struct timeval tv;
+	int ret;
 
 	gps_data = connect_to_gpsd(args);
 
@@ -37,7 +40,23 @@ void record_track(cmd_args args)
 		exit(-1);
 	}
 
-	/* Read data and write to file until user interrupts us*/
+	/* Read data and write to file until user interrupts us */
+	while (1) {
+		ret = gps_read(&gps_data);
+
+		if (ret < 0) {
+			fprintf(stderr, "gps_read error: %d\n", ret);
+			exit(1);
+		}
+
+		if ((gps_data.status == STATUS_FIX) &&
+			(gps_data.fix.mode == MODE_2D || gps_data.fix.mode == MODE_3D) &&
+			!isnan(gps_data.fix.latitude) &&
+			!isnan(gps_data.fix.longitude)) {
+			gettimeofday(&tv, NULL);
+			printf("height: %f, latitude: %f, longitude: %f, speed: %f, timestamp: %f\n", gps_data.fix.altitude, gps_data.fix.latitude, gps_data.fix.longitude, gps_data.fix.speed, gps_data.fix.time/*tv.tv_sec*/);
+		}
+	}
 
 	gps_close(&gps_data);
 }
