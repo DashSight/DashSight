7755ac05c2b377b98caec2790a0b903d5f2a228a
Alistair Francis <alistair@alistair23.me>
2018-07-11 22:03:19 -0700

    record-track: Try to get it working
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- record-track.c
+++ record-track.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <math.h>
 #include <gps.h>
 #include "common.h"
@@ -27,7 +28,6 @@ void record_track(cmd_args args)
 {
 	FILE *fd;
 	struct gps_data_t gps_data;
-	struct timeval tv;
 	int ret;
 
 	gps_data = connect_to_gpsd(args);
@@ -40,23 +40,37 @@ void record_track(cmd_args args)
 		exit(-1);
 	}
 
+	gps_stream(&gps_data, WATCH_ENABLE | WATCH_JSON, NULL);
+
 	/* Read data and write to file until user interrupts us */
 	while (1) {
-		ret = gps_read(&gps_data);
+		if (gps_waiting(&gps_data, 500)) {
+			ret = gps_read(&gps_data);
 
-		if (ret < 0) {
-			fprintf(stderr, "gps_read error: %d\n", ret);
-			exit(1);
+			if (ret < 0) {
+				fprintf(stderr, "gps_read error: %d\n", ret);
+				exit(1);
+			}
+
+			if (gps_data.set) {
+				printf("\n--- GPS ---\n");
+				printf("gps_data.online:           %10.0f\n", gps_data.online);
+				printf("gps_data.status:           %d\n", gps_data.status);
+				printf("gps_data.satellites_used:  %d\n", gps_data.satellites_used);
+				printf("gps_data.fix.mode:         %d\n", gps_data.fix.mode);
+				printf("gps_data.fix.time:         %10.0f\n", gps_data.fix.time);
+				printf("gps_data.fix.latitude:     %f\n", gps_data.fix.latitude);
+				printf("gps_data.fix.longitude:    %f\n", gps_data.fix.longitude);
+				printf("gps_data.fix.altitude:     %f\n", gps_data.fix.altitude);
+				printf("gps_data.fix.speed:        %f\n", gps_data.fix.speed);
+				printf("gps_data.fix.track:        %f\n", gps_data.fix.track);
+				printf("gps_data.dop.pdop:         %f\n", gps_data.dop.pdop);
+			}
 		}
 
-		if ((gps_data.status == STATUS_FIX) &&
-			(gps_data.fix.mode == MODE_2D || gps_data.fix.mode == MODE_3D) &&
-			!isnan(gps_data.fix.latitude) &&
-			!isnan(gps_data.fix.longitude)) {
-			gettimeofday(&tv, NULL);
-			printf("height: %f, latitude: %f, longitude: %f, speed: %f, timestamp: %f\n", gps_data.fix.altitude, gps_data.fix.latitude, gps_data.fix.longitude, gps_data.fix.speed, gps_data.fix.time/*tv.tv_sec*/);
-		}
+		sleep(2);
 	}
 
+	gps_stream(&gps_data, WATCH_DISABLE, NULL);
 	gps_close(&gps_data);
 }
