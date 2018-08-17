229f61bf5362c105af0523924eb416447e672151
Alistair Francis <alistair@alistair23.me>
2018-08-17 03:28:15 +0000

    lap-timer: Fix the subtract function
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- lap-timer.c
+++ lap-timer.c
@@ -32,19 +32,12 @@ struct timespec timeval_subtract(struct timespec *x, struct timespec *y)
 	struct timespec result;
 	int nsec;
 
-	if (x->tv_nsec < y->tv_nsec) {
-		nsec = (y->tv_nsec - x->tv_nsec) / 1000 + 1;
-		y->tv_nsec -= 1000 * nsec;
-		y->tv_sec += nsec;
-	}
-	if (x->tv_nsec - y->tv_nsec > 1000) {
-		nsec = (x->tv_nsec - y->tv_nsec) / 1000;
-		y->tv_sec += 1000 * nsec;
-		y->tv_sec -= nsec;
-	}
-
 	result.tv_sec = x->tv_sec - y->tv_sec;
-	result.tv_nsec = x->tv_nsec - y->tv_nsec;
+
+	if ((result.tv_nsec = x->tv_nsec - y->tv_nsec) < 0) {
+		result.tv_nsec += 1000000000;
+		result.tv_sec--;
+	}
 
 	return result;
 }
