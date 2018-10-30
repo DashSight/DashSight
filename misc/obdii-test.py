d461e2eda0537e4a1033601744362c77a55507cd
Alistair Francis <alistair@alistair23.me>
2018-10-30 04:49:41 +0000

    obdii-test.py: Try to print all commands
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- misc/obdii-test.py
+++ misc/obdii-test.py
@@ -1,13 +1,20 @@
 import sys
 import obd
+from obd import OBDStatus
 
 obd.logger.setLevel(obd.logging.DEBUG)
 
 connection = obd.OBD("/dev/ttyS0")
 
-cmd = obd.commands.RPM
+if connection.status() != OBDStatus.CAR_CONNECTED:
+    print "Unable to connect to the car"
+    # exit(1)
 
-response = connection.query(cmd)
+for pid in range(1, 0x5E):
+    cmd = obd.commands.has_pid(1, pid)
 
-print(response.value)
+    if connection.supports(cmd):
+        response = connection.query(cmd)
+        print "Car supports command pid: " + str(pid)
+        print(response.value)
 
