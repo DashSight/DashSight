b8b71a11e9172700251cacf5e5695966138de7a4
Alistair Francis <alistair@alistair23.me>
2018-10-30 20:02:54 -0700

    obdii-test.py: Some fixes
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- misc/obdii-test.py
+++ misc/obdii-test.py
@@ -4,17 +4,24 @@ from obd import OBDStatus
 
 obd.logger.setLevel(obd.logging.DEBUG)
 
-connection = obd.OBD("/dev/ttyS0")
+connection = obd.OBD("/dev/ttyS0", baudrate=9600, protocol=3, fast=False)
 
 if connection.status() != OBDStatus.CAR_CONNECTED:
     print "Unable to connect to the car"
-    # exit(1)
+    exit(1)
 
-for pid in range(1, 0x5E):
-    cmd = obd.commands.has_pid(1, pid)
+print "Listing supported commands:"
+for c in obd.supported_commands:
+	print(str(c))
+
+print ""
+
+for pid in range(1, 0x20):
+    cmd = obd.commands[1, pid]
 
     if connection.supports(cmd):
         response = connection.query(cmd)
         print "Car supports command pid: " + str(pid)
+        print(response.message)
         print(response.value)
 
