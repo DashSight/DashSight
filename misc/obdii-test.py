88f47119d90847796a7358e651348f10bca63f80
Alistair Francis <alistair@alistair23.me>
2018-11-04 21:37:46 +0000

    obdii-test.py: Add what kind of works
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- misc/obdii-test.py
+++ misc/obdii-test.py
@@ -1,27 +1,28 @@
 import sys
+import time
 import obd
 from obd import OBDStatus
 
 obd.logger.setLevel(obd.logging.DEBUG)
 
-connection = obd.OBD("/dev/ttyS0", baudrate=9600, protocol=3, fast=False)
+connection = obd.OBD("/dev/ttyS2", baudrate=9600, protocol="3", fast=False)
 
 if connection.status() != OBDStatus.CAR_CONNECTED:
-    print "Unable to connect to the car"
+    print("Unable to connect to the car")
     exit(1)
 
-print "Listing supported commands:"
-for c in obd.supported_commands:
-	print(str(c))
-
-print ""
-
-for pid in range(1, 0x20):
-    cmd = obd.commands[1, pid]
-
-    if connection.supports(cmd):
-        response = connection.query(cmd)
-        print "Car supports command pid: " + str(pid)
-        print(response.message)
+print("Listing supported commands:")
+print(connection.supported_commands)
+for c in connection.supported_commands:
+        print(str(c))
+        response = connection.query(c)
         print(response.value)
+
+#for pid in range(1, 0x20):
+#    time.sleep(3)
+#    cmd = obd.commands[1, pid]
+#
+#    response = connection.query(pid)
+#    print("Car supports command pid: " + str(pid))
+#    print(response.value)
 
