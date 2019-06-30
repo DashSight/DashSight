6415dd5a248968262389099589a9f98f2befda4b
Alistair Francis <alistair@alistair23.me>
2019-06-30 12:20:53 -0700

    obdii_connect.py: Use spaces instead of tabs
    This makes it easier to edit with vim on the target.
    
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- src/obdii_connect.py
+++ src/obdii_connect.py
@@ -19,51 +19,51 @@ from obd import OBDStatus
 # obd.logger.setLevel(obd.logging.DEBUG)
 
 class LapTimerOBD(object):
-	def __init__(self):
-		self.connection = obd.OBD("/dev/ttyS3", start_low_power=True)
+    def __init__(self):
+        self.connection = obd.OBD("/dev/ttyS3", start_low_power=True)
 
-		if self.connection.status() != OBDStatus.CAR_CONNECTED:
-			print("Unable to connect to the car")
-			exit(1)
+        if self.connection.status() != OBDStatus.CAR_CONNECTED:
+            print("Unable to connect to the car")
+            exit(1)
 
-	def get_data(self, cmd):
-		"""
-			Get the current OBDII data and increment the command.
+    def get_data(self, cmd):
+        """
+            Get the current OBDII data and increment the command.
 
-			Returns -1 on failure. On success the fail returned by the
-			car is returned.
-		"""
-		if self.connection.status() != OBDStatus.CAR_CONNECTED:
-			print("No connection to car")
-			return -1
+            Returns -1 on failure. On success the fail returned by the
+            car is returned.
+        """
+        if self.connection.status() != OBDStatus.CAR_CONNECTED:
+            print("No connection to car")
+            return -1
 
-		ret = self.connection.query(obd.commands[cmd])
+        ret = self.connection.query(obd.commands[cmd])
 
-		try:
-			ret = ret.value.magnitude
-		except:
-			ret = ret.value
+        try:
+            ret = ret.value.magnitude
+        except:
+            ret = ret.value
 
-		return ret
+        return ret
 
-	def enter_low_power(self):
-		"""
-			Enter low power mode
+    def enter_low_power(self):
+        """
+            Enter low power mode
 
-			Returns 0 on success and -1 on failure.
-		"""
-		line = self.connection.low_power()
+            Returns 0 on success and -1 on failure.
+        """
+        line = self.connection.low_power()
 
-		if 'OK' in lines:
-			return 0
+        if 'OK' in lines:
+            return 0
 
-		return -1
+        return -1
 
 def c_get_data(com):
-	return lap_timer.get_data(com)
+    return lap_timer.get_data(com)
 
 def c_enter_low_power(com):
-	return lap_timer.enter_low_power()
+    return lap_timer.enter_low_power()
 
 lap_timer = LapTimerOBD()
 
