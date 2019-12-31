db7f1a53fccb8b65b3168d405b8f54aea0fa7e48
Alistair Francis <alistair@alistair23.me>
2019-12-30 22:35:48 -0800

    src: Keep working on the Rust conversion
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,12 @@ extern crate gdk;
 
 use gtk::prelude::*;
 use gio::prelude::*;
-use gdk::prelude::*;
 
 use gtk::{Box, ButtonBox, Image};
 
 use std::env;
+
+mod track;
 
 fn main() {
     let uiapp = gtk::Application::new(
@@ -45,19 +46,37 @@ fn main() {
         gtk::Container::add(&button_box.clone().upcast::<gtk::Container>(), &record_button);
         gtk::Button::set_always_show_image(&record_button, true);
         gtk::Button::set_image(&record_button, Some(&record_button_image));
+        record_button.connect_clicked(|_| {
+            track::record::button_press_event()
+        });
+
+        let drive_button_image = Image::new_from_file("DriveTrack.png");
+        let drive_button = gtk::Button::new_with_label("Drive a single line");
+        gtk::Container::add(&button_box.clone().upcast::<gtk::Container>(), &drive_button);
+        gtk::Button::set_always_show_image(&drive_button, true);
+        gtk::Button::set_image(&drive_button, Some(&drive_button_image));
+        drive_button.connect_clicked(|_| {
+            track::line::button_press_event()
+        });
+
+        let close_button = gtk::Button::new_with_label("Close!");
+        gtk::Container::add(&button_box.clone().upcast::<gtk::Container>(), &close_button);
+        // close_button.connect_clicked(|_| {
+        //     win.close();
+        // });
 
         let main_page = Box::new(gtk::Orientation::Vertical, 0);
 
         let main_image = Image::new_from_file("SplashPage.png");
-		Box::pack_start(&main_page,
-						&main_image,
-						true, true, 0);
+        Box::pack_start(&main_page,
+                        &main_image,
+                        true, true, 0);
 
-		Box::pack_start(&main_page,
-						&button_box,
-						true, true, 0);
+        Box::pack_start(&main_page,
+                        &button_box,
+                        true, true, 0);
 
-		gtk::Container::add(&win.clone().upcast::<gtk::Container>(), &main_page);
+        gtk::Container::add(&win.clone().upcast::<gtk::Container>(), &main_page);
 
         win.show_all();
     });
