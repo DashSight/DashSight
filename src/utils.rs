1e15cab7493d12eb3a896eaebcf152d3efda534d
Alistair Francis <alistair@alistair23.me>
2020-04-20 21:56:18 -0700

    utils: Add some unit tests
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- src/utils.rs
+++ src/utils.rs
@@ -87,3 +87,14 @@ pub fn get_gps_lat_lon(reader: &mut dyn io::BufRead) -> Result<(f64, f64, f32, S
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_comparison() {
+        assert_eq!(lat_lon_comp(10.0, 10.0, 10.0, 10.0), true);
+        assert_eq!(lat_lon_comp(10.0, 10.0, 15.0, 15.0), false);
+    }
+}
